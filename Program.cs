9448a489576a118cbb5262f3bc0c8c68c4b9d02d
sillyweee <johsil15@student.westerdals.no>
2016-11-20 17:36:41 +0100

    in order

--- Program.cs
+++ Program.cs
@@ -31,14 +31,17 @@ namespace CookieBakery
     class Program
     {
         static Queue<int> cookies = new Queue<int>();
-        static Random rand = new Random(50);
-        const int CookieThreads = 3;
+        static Random rand = new Random(0);
+        const int CookieThreads = 1;
         static int[] sums = new int[CookieThreads];
+
+        // The Lock placed here so we only get one at a time
+
         static void ProduceCookies()
         {
-            for (int i = 1; i < 24; i++)
+            for (int i = 1; i < 20; i++)
             {
-                int cookieToEnqueue = rand.Next(10);
+                int cookieToEnqueue = i;
                 Console.WriteLine("Bakery made cookie #" + cookieToEnqueue);
                 lock (cookies)
                     cookies.Enqueue(cookieToEnqueue);
@@ -46,30 +49,45 @@ namespace CookieBakery
             }
 
         }
-        static void SumCookies(Object threadCookie)
 
+        // This is the original lock
+
+        static void Customer(Object threadCookie)
+            //= rand.Next(10)
         {
+            
             DateTime startTime = DateTime.Now;
             int mySum = 0;
             while ((DateTime.Now - startTime).Seconds < 11)
             {
-                int cookieToSum = -1;
-                lock (cookies)
+                int cookieToSum = 0;
+                  lock (cookies)
                 {
                     if (cookies.Count != 0)
                     {
                         cookieToSum = cookies.Dequeue();
                     }
                 }
-                if (cookieToSum != -1)
+                if (cookieToSum != 0)
                 {
                     mySum += cookieToSum;
                     Console.WriteLine(" recived Bakerys Cookie #" + cookieToSum);
+                    Thread.Sleep(300);
 
                 }
             }
-            sums[(int)threadCookie] = mySum;
         }
+
+
+
+/**
+* This is the last step, 
+* 
+* These starts up the application CookieBakery
+* 
+* 
+* And closes it after 
+* */
         static void Main()
         {
             var producingThread = new Thread(ProduceCookies);
@@ -77,17 +95,34 @@ namespace CookieBakery
             Thread[] threads = new Thread[CookieThreads];
             for (int i = 0; i < CookieThreads; i++)
             {
-                threads[i] = new Thread(SumCookies);
+                threads[i] = new Thread(Customer);
                 threads[i].Start(i);
             }
-            for (int i = 1; i < CookieThreads; i++)
-                threads[i].Join();
-            int totalSum = 0;
-            for (int i = 1; i < CookieThreads; i++)
-                totalSum += sums[i];
-            Console.WriteLine("Bakery closed for the day" + totalSum);
-            Console.ReadKey( );
-        }
 
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
     }
 }
+/**
+        class MySychonizedQueue<T>
+        {
+            Queue<T> theQ = new Queue<T>();
+            public void Enqueue(T item)
+            {
+                lock (theQ)
+                    theQ.Enqueue(item);
+            }
+            public T Dequeue()
+            {
+                lock (theQ)
+                    return theQ.Dequeue();
+            }
+            public int Count
+            {
+                get { lock (theQ) return theQ.Count; }
+            }
+        }
+
+**/
